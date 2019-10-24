d27dc49756987cc457db5b0707d7d8a1e55da48e
davidhdez98 <47402898+davidhdez98@users.noreply.github.com>
2019-10-24 21:31:19 +0100

    Add files via upload

---
+++ Practica03/Scripts/fisica.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fisica : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Rigidbody rb;
+    public float avance;
+    public float choques_azul = 0;
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        avance = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(new Vector3(1, 0, 0) * avance, ForceMode.Acceleration);  
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(new Vector3(-1, 0, 0) * avance, ForceMode.Acceleration);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(new Vector3(0, 0, 1) * avance, ForceMode.Acceleration);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(new Vector3(0, 0, -1) * avance, ForceMode.Acceleration);
+        }
+
+    }
+
+        private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstaculo" || collision.gameObject.tag == "Player")
+        {
+            choques_azul = choques_azul + 1;    
+            
+        } 
+    }
+
+    
+}
+    
---
+++ Practica03/Scripts/sensores.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sensores : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstaculo" || other.gameObject.tag == "Player")
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstaculo" || collision.gameObject.tag == "Player")
+        {
+            GetComponent<Renderer>().material.color = new Color32(255, 123, 4, 246);       
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstaculo" || collision.gameObject.tag == "Player")
+        {
+            GetComponent<Renderer>().material.color = Color.red;         
+        }
+    }
+
+    
+}
